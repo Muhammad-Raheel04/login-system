3c49978c3ed576c39d3f9e867eda2f0664c51182
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-30 21:08:16 +0500

    used mongodb module to configure and connect with mongodb atlas

--- db.js
+++ db.js
@@ -1,3 +1,14 @@
 const {MongoClient} = require('mongodb');
 
 const client =new MongoClient(process.env.MONGO_URL);
+
+let db;
+async function connectDB(){
+    if(!db){
+        await client.connect();
+        db=client.db(process.env.DB_NAME);
+        console.log("MongoDB connected");
+    }
+    return db;
+}
+module.exports=connectDB;
