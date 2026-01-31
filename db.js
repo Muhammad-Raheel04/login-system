381787f562a870630503abe5c8e6f5632f279299
Muhammad Raheel <raheel31102004@gmail.com>
2026-01-31 10:00:10 +0500

    updated mongoDB connection for production added tls true to ensure secure TLS connection for atlas to support connectDB.js to support production environment

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const client =new MongoClient(process.env.MONGO_URL);
+
 
 let db;
-async function connectDB(){
-    if(!db){
-        await client.connect();
-        db=client.db(process.env.DB_NAME);
-        console.log("MongoDB connected");
+async function connectDB() {
+    if (db) {
+        return db;
     }
+    const client = new MongoClient(process.env.MONGO_URL, {
+        tls: true
+    });
+
+    await client.connect();
+    db = client.db(process.env.DB_NAME || 'users');
+    console.log("MongoDB connected");
     return db;
 }
-module.exports=connectDB;
+module.exports = connectDB;
