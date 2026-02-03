eb81b4de595097f983f30d36594f9bcadb1148d3
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-04 01:52:24 +0500

    updated signUp js code to execute only when dom gets loaded and added password validation and interactive behavoir to submit btn

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,10 +1,26 @@
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const submitBtn =document.getElementById('submit');
+window.addEventListener('DOMContentLoaded', () => {
+    const username = document.getElementById('username');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const submitBtn = document.getElementById('submit');
+    const form = document.getElementById('signupForm');
 
-submitBtn.addEventListener('click',(e)=>{
-    if(password.value.length<=8){
-        e.preventDefault();
-        alert("Password must be of length 8");
-    }
+    form.addEventListener('submit', (e) => {
+       
+
+        submitBtn.disabled = true;
+        submitBtn.innerText = "Checking...";
+
+        if (password.value.length < 8) {
+            e.preventDefault(); // block only if submission fails
+            submitBtn.innerText = "Password must be of at least 8 characters";
+            setTimeout(() => {
+                submitBtn.innerText = 'Sign Up';
+                submitBtn.disabled = false;
+            }, 3000)
+            return;
+        }
+        submitBtn.innerText = "Signing you up...";
+        submitBtn.disabled = true;
+    })
 })
