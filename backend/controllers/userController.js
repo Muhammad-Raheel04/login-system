01e12f8b90290b8517cf5e1492579cd9324c48be
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-17 22:00:03 +0500

    hash passwords with bcrypt on user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+import { User } from "../models/userModel.js";
+import bcrypt from 'bcrypt';
 export const register=async(req,res)=>{
     try{
         const {firstName,lastName,email,password}=req.body;
@@ -7,9 +9,25 @@ export const register=async(req,res)=>{
                 message:"All field are required"
             })
         }
-        return res.json({
+        const user=await User.findOne({email});
+        if(user){
+            return res.status(400).json({
+                success:false,
+                message:"User already exist",
+            })
+        }
+        const hashedPassword=await bcrypt.hash(password,10);
+        const newUser=await User.create({
+            firstName,
+            lastName,
+            password:hashedPassword,
+            email,
+        })
+        await newUser.save();
+        return res.status(201).json({
             success:true,
-            message:"User Registerd successfully"
+            message:"User Registerd successfully",
+            newUser,
         })
     }catch(error){
         res.status(500).json({
