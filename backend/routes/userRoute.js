893cc53e3bffd2a0a1813829a86a0a0edbc3bb1d
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-18 22:27:54 +0500

    add login and logout post routes in user routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { register, verify } from '../controllers/userController.js';
+import { login, logout, register, verify } from '../controllers/userController.js';
 
 const router=express.Router();
 
 router.post('/register',register);
 router.post('/verify',verify);
+router.post('/login',login);
+router.post('/logout',logout);
 
 export default router;
