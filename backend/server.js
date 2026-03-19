604e04004ac1b7b6681ff7acfdb18657afbd1e9f
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-19 19:57:41 +0500

    integrate cors middleware

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ import 'dotenv/config'
 import express from 'express';
 import userRoute from './routes/userRoute.js';
 import connectDB from './database/db.js';
+import cors from 'cors';
 const app =express();
 app.use(express.json());
 
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true 
+}));
 app.use('/api/v1/user',userRoute);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
