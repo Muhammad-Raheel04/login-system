a725fcf3c5aae05610389dec3f75e4890e14bd1c
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-19 14:26:55 +0500

    configure shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Button } from './components/ui/button'
 
 const App = () => {
   return (
     <div>
-      
+      <Button variant='default' className='!bg-pink-300 !text-2xl'>Click Me</Button>
     </div>
   )
 }
